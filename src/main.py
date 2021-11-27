98ef92b8acd5a3fe744f0f1b9e6697c2c91e11d1
isabellahandayani <isabellahandayani@gmail.com>
2021-11-27 19:26:52 +0700

    fix rollback

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import re
-import sys 
+import sys
 
 from const import *
 from mvcc.manager import *
@@ -10,11 +10,10 @@ def main():
     file = open(sys.argv[1], "r")
     text = list(file)
     text[len(text) - 1] += "\n"
-    text = [s.strip().lower() for s in text]
+    text = [s.strip() for s in text]
     manager = Manager()
     manager._queue = text
     manager.run()
-    manager.result()
 
 
 if __name__ == "__main__":
--- src/mvcc/manager.py
+++ src/mvcc/manager.py
@@ -2,17 +2,18 @@ from mvcc.resource import Resource
 from mvcc.transaction import *
 from const import *
 from util import *
+
 import re
+import logging
 
 
 class Manager:
     def __init__(self):
-        self._transaction = []
+        self._transaction = {}
         self._queue = []
         self._resource = {}
         self._read_logs = {}
         self._write_logs = {}
-        self._ts = 1
 
     def is_rollback(self, txn):
         return self.get_txn(txn - 1)._status == Status.ABORT
@@ -31,7 +32,7 @@ class Manager:
 
     def get_txn(self, key):
         try:
-            return self._transaction[key - 1]
+            return self._transaction[key]
         except:
             return False
 
@@ -42,8 +43,8 @@ class Manager:
         return len(self._transaction)
 
     def print_transaction(self):
-        for x in self._transaction:
-            print(x)
+        for key in self._transaction:
+            print(self._transaction[key])
 
     def print_resource(self):
         for x in self._resource:
@@ -51,7 +52,7 @@ class Manager:
 
     def check_write(self, txn):
         written = []
-        for x in self._write_logs[txn + 1]:
+        for x in self._write_logs[txn]:
             written.append(x)
         return written
 
@@ -66,9 +67,9 @@ class Manager:
 
     def get_aborted(self):
         arr = []
-        for x in self._transaction:
-            if x._status == Status.ABORT:
-                arr.append(x._ts)
+        for key in self._transaction:
+            if self._transaction[key]._status == Status.ABORT:
+                arr.append(key)
         return arr
 
     def rollback(self, read):
@@ -82,52 +83,65 @@ class Manager:
 
     def get_max(self, res, txn):
         curr = None
-        TS = self.get_txn(txn).get_ts()
         for vers in self.get_vers(res):
             res_ts = self.get_vers(res)[vers]
-            if res_ts[1] <= TS:
-                curr = res_ts[1]
-
+            if res_ts[1] <= txn:
+                curr = res_ts[0], res_ts[1], vers
         return curr
 
     def req_write(self, res, txn, val):
+        logging.info(f"[WRITE] T{txn} {res} {val}")
         TS = self.get_txn(txn).get_ts()
         qk = self.get_max(res, txn)
         # Rollback
-        if qk is None or TS < self.get_vers(res)[qk][0]:
+        if TS < qk[0]:
+            logging.info(f"[ABORT] T{TS}")
             self.get_txn(txn).abort()
             self.rollback(self.check_read(self.check_write(txn)))
         # Overwrite
-        elif TS == self.get_vers(res)[qk][1]:
-            self._resource[res]._version[TS][2] = val
+        elif TS == qk[1]:
+            logging.info(f"[Overwrite] {res} Version{TS}")
+            self._resource[res]._version[qk[3]][2] = val
             self.set_write(txn, qk, res, TS)
         # Add new version
         else:
+            logging.info(f"[WRITE] {res} Version {TS}")
             self._resource[res]._version[TS] = [TS, TS, val]
             self.set_write(txn, TS, res, TS)
 
     def req_read(self, res, txn):
-        TS = self.get_txn(txn).get_ts()
+        """
+        Request Read 
+        """
+        
         qk = self.get_max(res, txn)
-        if qk is not None:
-            self.get_vers(res)[qk][0] = TS
-        self.set_read(txn, qk, res, TS)
+        logging.info(f"[READ] T{txn} {res}")
+        
+        # If TS > R-TS QK 
+        if txn > qk[0]:
+            self.get_vers(res)[qk[2]][0] = txn
+        self.set_read(txn, qk, res, txn)
 
     def run(self):
+        logging.basicConfig(
+            format=" %(message)s", datefmt="[%H:%M:%S]", level=logging.INFO
+        )
         while len(self._queue) > 0:
+            logging.info(f"[!] {self._queue[0]}")
             if int(getTrans(self._queue[0])) in self.get_aborted():
+                logging.info(f"[Warning]: Transaction already rollbacked, skipped ")
                 self._queue.pop(0)
             else:
-                # Begin
                 if not self.get_txn(getNumber(self._queue[0])):
                     txn = Transaction(
-                        self.get_last_idx(), getNumber(self._queue[0]), self._ts
+                        self.get_last_idx(),
+                        getNumber(self._queue[0]),
+                        getNumber(self._queue[0]),
                     )
-                    self._ts += 1
-                    self._transaction.append(txn)
+                    self._transaction[getNumber(self._queue[0])] = txn
 
                 # W/R
-                if re.search(Pattern.WRITE, self._queue[0]) is not None:
+                if re.search(Pattern.WRITE, self._queue[0], re.IGNORECASE) is not None:
                     params = getParam(self._queue[0])
                     params = [s.strip() for s in params]
                     if params[1] not in self._resource:
@@ -135,7 +149,7 @@ class Manager:
 
                     self.req_write(params[1], getNumber(params[0]), params[2])
 
-                elif re.search(Pattern.READ, self._queue[0]) is not None:
+                elif re.search(Pattern.READ, self._queue[0], re.IGNORECASE) is not None:
                     params = getParam(self._queue[0])
                     params = [s.strip() for s in params]
                     if params[1] not in self._resource:
@@ -145,10 +159,9 @@ class Manager:
                     self.req_read(params[1], getNumber(params[0]))
 
                 # Commit
-                elif re.search(Pattern.COMMIT, self._queue[0]) is not None:
-                    self._transaction[getNumber(self._queue[0]) - 1].commit()
+                elif (
+                    re.search(Pattern.COMMIT, self._queue[0], re.IGNORECASE) is not None
+                ):
+                    self._transaction[getNumber(self._queue[0])].commit()
                 self._queue.pop(0)
-
-    def result(self):
-        for x in self._transaction:
-            print(x)
+        self.print_resource()
